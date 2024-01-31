0872f8f14846fa4a3411113eeef20d7494edccca
artem_egorov <artegtor@gmail.com>
2024-01-31 20:25:51 +0500

    add grid cats

--- src/components/Cats/Cats.tsx
+++ src/components/Cats/Cats.tsx
@@ -3,10 +3,16 @@ import styles from "./Cats.module.scss";
 import { useAppDispatch, useAppSelector } from "@/store/hook";
 import { fetchCatsAction } from "@/store/slices/catsSlice";
 import { CatItem } from "./CatItem/CatItem";
+import { Loader } from "@/ui/Loader/Loader";
 
 export const Cats = () => {
   const dispatch = useAppDispatch();
-  const { cats } = useAppSelector((state) => state.catsSlice);
+  const {
+    cats,
+    isLoadings: { isFetchCatsLoading, isFetchMoreCatsLoading },
+    errors: { fetchCatsErr, fetchMoreCatsErr },
+  } = useAppSelector((state) => state.catsSlice);
+  const isLoading = isFetchCatsLoading || isFetchMoreCatsLoading;
 
   useEffect(() => {
     if (cats?.length === 0) {
@@ -15,12 +21,18 @@ export const Cats = () => {
   }, [cats, dispatch]);
 
   return (
-    <div>
-      <ul className={styles.cats}>
-        {cats.map((cat) => (
-          <CatItem key={cat.id} cat={cat} />
-        ))}
-      </ul>
+    <div className="container">
+      {isLoading && <Loader />}
+      {fetchCatsErr && <p className="center">Error: {fetchCatsErr}</p>}
+      {!isLoading && !fetchCatsErr && !fetchMoreCatsErr && cats.length === 0 ? (
+        <p className="center">No cats found</p>
+      ) : (
+        <ul className={styles.cats}>
+          {cats.map((cat) => (
+            <CatItem key={cat.id} cat={cat} />
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
